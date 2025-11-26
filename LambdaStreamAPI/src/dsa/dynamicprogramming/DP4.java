14e78c5548a93dcaa848c42b7ff6e2175ff78f47
rdgupta93 <rdgupta1336@gmail.com>
2025-11-26 07:45:35 +0530

    Code of SuperSrquence and Edit distance in DP

--- LambdaStreamAPI/src/dsa/dynamicprogramming/DP4.java
+++ LambdaStreamAPI/src/dsa/dynamicprogramming/DP4.java
@@ -191,7 +191,110 @@ public class DP4 {
         int lengthOfLongSubsPalind =longestPalindSubsequence(s5,rev5,n-1,n-1,dp5);
         int minDel =n5-lengthOfLongSubsPalind;
         System.out.println("Minimum Number of Deletions to Make a String Palindrome: "+minDel);
+
+        /*
+        Question 6:- Shortest Common Supersequence (SCS)
+        What is the problem?
+        You are given two strings:
+        str1 = "abac"
+        str2 = "cab"
+        You must build the shortest possible string that contains both str1 and str2 as subsequences.
+
+        What does Supersequence mean?
+        A string S is a supersequence of str1 if:
+        - You can derive str1 from S by deleting some characters.
+        - Same for str2.
+        S = "cabac"
+        This contains both:
+        str1 = "abac"
+        str2 = "cab"
+
+        Find the Shortest Common Supersequence (SCS).
+        Length of SCS = len(str1) + len(str2) – LCS(str1, str2)
+
+        SCS is basically:
+        Add non-matching characters from both strings +
+        Add matching characters (LCS) only once.
+         */
+        String s16 ="abac";
+        String s26 ="cab";
+        // cal length of longest common subsequence
+        char []ch16 = s16.toCharArray();
+        char []ch26 = s26.toCharArray();
+        int dp6[][] = new int[ch16.length][ch26.length];
+        for(int i=0;i<ch16.length;i++){
+            for(int j=0;j< ch26.length;j++){
+                dp6[i][j]=-1;
+            }
+        }
+        int longestLCS =LCS(ch16,ch26,0,0,dp6);
+        // Length of SCS = len(str1) + len(str2) – LCS(str1, str2)
+        int ans6 = s16.length()+s26.length()-longestLCS;
+        System.out.println("Shortest Common Supersequence: "+ans6);
+
+        int n6 =s16.length();
+        int m6 =s26.length();
+        String dp61[][] = new String[n6][m6];
+        String scs =shortestCommonSuperSequence(s16,s26,0,0,dp61);
+        System.out.println("Shortest Common Supersequence: "+scs);
+
+        // Shortest common superSequence using backtracking
+
+        String scs1 =shortestCommonSuperSequenceI(s16,s26);
+        System.out.println("Shortest Common SuperSequence using backtracking: "+scs1);
+
+        /*
+        Question 7 :- Edit Distance / Levenshtein Distance
+        Edit Distance = Minimum number of operations needed to convert one string into another
+        Allowed operations:
+        Insert a character : i - we can insert any char in s1 at any pos
+        Delete a character : d - we can delete any char in s1 at any pos
+        Replace a character: r - we can replace any char in s1 at any pos , with any char
+        s1: l e a f d
+        s2: l g f
+        s1-> delete e  , replace a with g , delete d  --> 3 ops
+
+                     edit (s1[0-4] s2[0-2])
+                       if(s1[0] ==s2[0]) // char match
+                     edit(s1[1-4]s2[1-2)
+                     s1[1]!=s2[1]     // if char not matched
+
+          ops: insert                  ops :replace                      ops:delete
+              1  2  3  4  5
+          s1: l g e  g  f  d            e is replace with g              e is deleted
+          s2: l  g  f
+          edit(s1[1-4]s2[2-2])+1       edit(s1[2-4]s2[2-2])+1            edit(s1[2-4]s2[1-2])+1
+          // hypothetically insert g
+          hence s1[1-4]
+
+
+          Base cond:
+          if(i==N && j==M){
+          return 0;            // Both are exhausted ;s1="",s2=""
+          }
+          if(i==n){// s1=""; s1 is exhausted
+          return m-j // s2= j j+1 ....m-1 ;  b-a +1 = m-1+j+1 =m-j
+          }
+          if(j==m){ //s2=""; s2 is exhausted
+          return n-i; s1=i i+1 .......n-1; n-i;
+          }
+
+       Question 7:
+         */
+
+        String s7 ="leafd";
+        String s71="lgf";
+        int dp7[][] = new int[s7.length()+1][s71.length()+1];
+        for(int i=0;i<=s7.length();i++){
+            for(int j=0;j<=s71.length();j++){
+                dp7[i][j]=-1;
+            }
+        }
+        int minOps = editDistance(s7.toCharArray(),s71.toCharArray(),0,0,dp7);
+        System.out.println("Min Ops required to edit distance: "+minOps);
     }
+
+
 
     private static int LCS(char [] s1,char [] s2,int i,int j,int dp [][]){
         if(i==s1.length || j==s2.length){
@@ -355,6 +458,124 @@ public class DP4 {
             }
         }
         return dp5[i][j];
+    }
+
+    private static String shortestCommonSuperSequence(String s1,String s2,int i,int j,String dp[][]){
+
+        // s1 is empty and take s2
+        if(i==s1.length()){
+            return s2.substring(j);
+        }
+        // s2 is empty and take s1
+        if(j==s2.length()){
+            return s1.substring(i);
+        }
+
+
+        if(dp[i][j]==null){
+            if(s1.charAt(i)==s2.charAt(j)){
+                dp[i][j]=s1.charAt(i)+shortestCommonSuperSequence(s1,s2,i+1,j+1,dp);
+            }else{
+                String option1= shortestCommonSuperSequence(s1,s2,i+1,j,dp);
+                String option2 =shortestCommonSuperSequence(s1,s2,i,j+1,dp);
+                if(option1.length()<= option2.length()){
+                    dp[i][j]=s1.charAt(i)+option1;
+                }else{
+                    dp[i][j]=s2.charAt(j)+option2;
+                }
+            }
+        }
+
+        return dp[i][j];
+        /*
+        Why do we add a character when characters don't match?
+        Because Shortest Common Supersequence (SCS) means:
+        You must include ALL characters of both strings
+        And keep the original order
+        But produce the shortest possible combined string
+        When characters do not match, you must include one of them.
+        If characters differ:
+        s1[i] != s2[j]
+        Then SCS has 2 ways:
+        Option 1:
+        Take s1[i] + SCS of the rest of s1 and s2:s1[i] + SCS(i+1, j)
+        Option 2:
+        Take s2[j] + SCS of s1 and rest of s2: s2[j] + SCS(i, j+1)
+
+         */
+    }
+
+    private static String shortestCommonSuperSequenceI(String s1, String s2) {
+        int n =s1.length();
+        int m =s2.length();
+
+        int dp[][]= new int[n+1][m+1];
+
+        // step 1 =LCS dp build
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        //Step 2 = Back tracking build SCS
+        StringBuilder sb = new StringBuilder();
+        int i=n,j=m;
+        while(i>0 && j>0){
+            if(s1.charAt(i-1)==s2.charAt(j-1)){
+                sb.append(s1.charAt(i-1));
+                i--;
+                j--;
+            } else if (dp[i-1][j]>dp[i][j-1]) {
+                sb.append(s1.charAt(i-1));
+                i--;
+            }else{
+                sb.append(s2.charAt(j-1));
+                j--;
+            }
+        }
+        // add remaining char in sb from s1
+        while(i>0){
+            sb.append(s1.charAt(i-1));
+            i--;
+        }
+        //add remaining char in sb from s2
+        while(j>0){
+            sb.append(s2.charAt(j-1));
+            j--;
+        }
+
+        return sb.reverse().toString();
+    }
+
+
+    private static int editDistance(char[] s1, char[] s2, int i, int j,int dp[][]) {
+        int n =s1.length;
+        int m =s2.length;
+        if(i==n && j==m){
+            return 0;
+        }
+        if(i==n){
+            return m-j;
+        }
+        if(j==m){
+            return n-i;
+        }
+       if(dp[i][j]==-1) {
+           if (s1[i] == s2[j]) {
+               dp[i][j]= editDistance(s1, s2, i + 1, j + 1,dp);
+           } else {
+             int a =editDistance(s1,s2,i,j+1,dp)+1;// insert
+             int b =editDistance(s1,s2,i+1,j+1,dp)+1; //replace
+             int c =editDistance(s1,s2,i+1,j,dp)+1; // delete
+             dp[i][j]=Math.min(a,Math.min(b,c));
+           }
+       }
+       return dp[i][j];
     }
 
 }
